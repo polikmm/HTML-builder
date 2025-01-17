8b03c606ca1a6b3e80c760e15d54343ba30b4521
polikmm <polikmm@mail.ru>
2025-01-17 18:35:06 +0400

    feat: add copying assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -12,7 +12,7 @@ async function buildPage() {
     // create new html page with all components inside
 
     const components = await fsPromise.readdir(path.join(__dirname, "components"));
-    await fs.readFile(path.join(__dirname, "template.html"), "utf-8", (err,data) => {
+    await fs.readFile(path.join(__dirname, "template.html"), "utf-8", (err, data) => {
       if (err) throw err;
 
       for (let item of components) {
@@ -28,11 +28,12 @@ async function buildPage() {
         })
       }
     })
-    
+
     // merge styles into one css file
 
     const styles = await fsPromise.readdir(path.join(__dirname, "styles"), { withFileTypes: true });
     const mergedStyles = path.join(__dirname, "project-dist/style.css");
+    let data = "";
     for (let style of styles) {
       if (style.name.split(".")[1] !== "css") continue;
       const readableStream = fs.createReadStream(`${style.path}/${style.name}`, "utf-8");
@@ -43,6 +44,47 @@ async function buildPage() {
         })
       })
     }
+
+    // copy assets
+
+    async function copyDir() {
+      fs.mkdir(path.join(__dirname, "project-dist/assets"),
+        { recursive: true },
+        (err) => { if (err) throw err; }
+      )
+      const folders = await fsPromise.readdir(`${__dirname}/assets`,
+        { withFileTypes: true }
+      )
+      for (let folder of folders) {
+        if (folder.name === ".DS_Store") continue;
+        fs.mkdir(path.join(__dirname, `project-dist/assets/${folder.name}`),
+          { recursive: true },
+          (err) => { if (err) throw err; }
+        )
+        copyFiles(folder.path, folder.name);
+      }
+    }
+
+    async function copyFiles(path, name) {
+      const files = await fsPromise.readdir(`${path}/${name}`, { withFileTypes: true });
+      const copiedFiles = await fsPromise.readdir(`${__dirname}/project-dist/assets/${name}`,
+        { withFileTypes: true });
+      if (copiedFiles) {
+        for (let file of copiedFiles) {
+          fs.unlink(`${__dirname}/project-dist/assets/${name}/${file.name}`,
+            (err) => { if (err) throw err; }
+          )
+        }
+      }
+
+      for (let file of files) {
+        fs.copyFile(`${file.path}/${file.name}`,
+          `${__dirname}/project-dist/assets/${name}/${file.name}`,
+          (err) => { if (err) throw err; }
+        )
+      }
+    }
+    copyDir();
   } catch (err) {
     console.log(err);
   }
